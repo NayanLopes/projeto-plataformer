f1ad5d5277a2180d8a96144a6e9d8d05bb685a71
Vitor Nayan <vitornayanst@gmail.com>
2025-07-15 22:14:47 -0300

    aplicando tilesets

--- Assets/01_Scripts/Systems/SceneController.cs
+++ Assets/01_Scripts/Systems/SceneController.cs
@@ -34,6 +34,15 @@ public class SceneController : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
     public void Quit()
     {
         Application.Quit();
