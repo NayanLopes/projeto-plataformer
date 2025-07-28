ae2e9bf6b34e0ddbae74e8b39b03a7bc3a7aff70
Daniel Fernandes <danielferdsc@gmail.com>
2025-07-27 23:25:57 -0300

    corrige bug.

--- Assets/01_Scripts/Environment/BouncingObject.cs
+++ Assets/01_Scripts/Environment/BouncingObject.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BouncingObject : MonoBehaviour
-{
+public class BouncingObject : MonoBehaviour {
     [SerializeField] private bool disableOnTouch = false;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
+    private void OnTriggerEnter2D( Collider2D collision ) {
+        
+        // Verifica se o objeto que colidiu possui a tag do jogador.
         if (collision.gameObject.CompareTag(Config.Instance.playerTag))
         {
             PlayerBouncing playerBouncing = collision.gameObject.GetComponent<PlayerBouncing>();
             playerBouncing.OnTouchingBouncingObject();
 
-            if(disableOnTouch)
+            if (disableOnTouch)
                 gameObject.SetActive(false);
         }
     }
